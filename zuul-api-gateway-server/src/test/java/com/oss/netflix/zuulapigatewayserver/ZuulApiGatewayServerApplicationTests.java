5e536ff46e2952a403e1c1380781b9799280f50e
Joe Engle <jeengle@indiana.edu>
2021-01-18 16:55:32 -0500

    initial zuul

---
+++ zuul-api-gateway-server/src/test/java/com/oss/netflix/zuulapigatewayserver/ZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oss.netflix.zuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
