2db7da27799b76d3f9ba10508aadac58c8b33dfa
Joe Engle <jeengle@indiana.edu>
2021-01-18 13:33:44 -0500

    initial feign clien

--- currency-conversion/src/main/java/com/oss/netflix/currencyconversion/CurrencyConversionApplication.java
+++ currency-conversion/src/main/java/com/oss/netflix/currencyconversion/CurrencyConversionApplication.java
@@ -2,7 +2,11 @@ package com.oss.netflix.currencyconversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/** Feign can have issues importing.  If this occurs do a maven clean package
+ * and then reimport all maven projects **/
+@EnableFeignClients("com.oss.netflix.currencyconversion")
 @SpringBootApplication
 public class CurrencyConversionApplication {
 
