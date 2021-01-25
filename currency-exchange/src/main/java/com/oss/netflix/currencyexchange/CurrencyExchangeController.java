8dc21d3e9c48fbb19c8cf28a0301c6ba1a682769
Joe Engle <jeengle@indiana.edu>
2021-01-25 13:26:38 -0500

    initial add of mysql db with volume

--- currency-exchange/src/main/java/com/oss/netflix/currencyexchange/CurrencyExchangeController.java
+++ currency-exchange/src/main/java/com/oss/netflix/currencyexchange/CurrencyExchangeController.java
@@ -3,11 +3,18 @@ package com.oss.netflix.currencyexchange;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @Slf4j
+@Validated
 @RequiredArgsConstructor
 @RestController
 public class CurrencyExchangeController {
@@ -17,7 +24,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-
+        log.info("DEBUG repo size is {}", repo.count());
         ExchangeValue exchangeValue =
                 repo.findByFromAndTo(from, to);
 
@@ -30,5 +37,15 @@ public class CurrencyExchangeController {
 
 //        return new ExchangeValue(1L, from, to, 1.0,
 //                Integer.parseInt(env.getProperty("local.server.port")));
+    }
+
+    @GetMapping("/currency-exchange")
+    public List<ExchangeValue> findAll() {
+        return repo.findAll();
+    }
+
+    @PostMapping("/currency-exchange")
+    public void save(@Valid @RequestBody ExchangeValue exchangeValue) {
+        repo.save(exchangeValue);
     }
 }
