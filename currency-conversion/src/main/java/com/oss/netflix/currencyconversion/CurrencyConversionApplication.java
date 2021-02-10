371bb9f8f2309606cde46baaacc11afc0e8d0671
Joe Engle <jeengle@indiana.edu>
2021-02-10 13:34:32 -0500

    refactor from zuul to spring cloud gateway

--- currency-conversion/src/main/java/com/oss/netflix/currencyconversion/CurrencyConversionApplication.java
+++ currency-conversion/src/main/java/com/oss/netflix/currencyconversion/CurrencyConversionApplication.java
@@ -3,13 +3,11 @@ package com.oss.netflix.currencyconversion;
 import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 /** Feign, Ribbon, Eureka can have issues importing.  If this occurs do a maven clean package
  * and then reimport all maven projects **/
-@EnableDiscoveryClient
 @EnableFeignClients("com.oss.netflix.currencyconversion")
 @SpringBootApplication
 public class CurrencyConversionApplication {
--- currency-exchange/src/main/java/com/oss/netflix/currencyexchange/CurrencyExchangeApplication.java
+++ currency-exchange/src/main/java/com/oss/netflix/currencyexchange/CurrencyExchangeApplication.java
@@ -3,10 +3,8 @@ package com.oss.netflix.currencyexchange;
 import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class CurrencyExchangeApplication {
 
